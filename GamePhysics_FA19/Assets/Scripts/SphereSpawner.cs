af2ab26b108d1396e2e8a0398e620e25f9c6380a
Grazda <josh.grazda@CHAMPLAIN.EDU>
2019-08-29 12:11:00 -0400

    Unity Project Committed
    - Unity Project 2019.2.2f

---
+++ GamePhysics_FA19/Assets/Scripts/SphereSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereSpawner : MonoBehaviour
+{
+    [Header("Sphere Spawner")]
+    public GameObject sphereSpawnerCenter;
+    [Range(-100,100)]
+    public int minSpawnRange;
+    [Range(-100, 100)]
+    public int maxSpawnRange;
+
+    [Header("Sphere")]
+    public GameObject spherePrefab;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnSphere();
+        }
+    }
+
+    void SpawnSphere()
+    {
+        Instantiate(spherePrefab, new Vector3(Random.Range(minSpawnRange, maxSpawnRange), sphereSpawnerCenter.transform.position.y, Random.Range(minSpawnRange, maxSpawnRange)), Quaternion.identity, sphereSpawnerCenter.transform);
+    }
+}
