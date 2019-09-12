dca84680d4b4138f68c433a4f89248b50f278433
Josh_J101_20 <joshuagrazda@gmail.com>
2019-09-12 12:14:36 -0400

    Continued working on Force Functions
    Changed:
    - Working on Force Functions in J_Force

--- GamePhysics_FA19/Assets/Scripts/Physics/J_Force.cs
+++ GamePhysics_FA19/Assets/Scripts/Physics/J_Force.cs
@@ -13,23 +13,30 @@ public class J_Force
     }
 
     // Vector2 GenerateForce_normal(Vector2 f_gravity, Vector2 surfaceNormal_unit);
-    public static Vector2 GenerateForce_Normal()
+    public static Vector2 GenerateForce_Normal(Vector2 f_gravity, Vector2 surfaceNormal_unit)
     {
-        // f_normal = proj(f_gravity, surfaceNormal_unit)
-        return Vector2.zero;
+        // f_normal = proj(-f_gravity, surfaceNormal_unit)
+        Vector2 f_normal = Vector3.Project(new Vector3(-f_gravity.x, 0.0f, -f_gravity.y), new Vector3(surfaceNormal_unit.x, 0.0f, surfaceNormal_unit.y));
+        return f_normal;
     }
 
     // Vector2 GenerateForce_sliding(Vector2 f_gravity, Vector2 f_normal);
-    public static Vector2 GenerateForce_Sliding()
+    public static Vector2 GenerateForce_Sliding(Vector2 f_gravity, Vector2 f_normal)
     {
         // f_sliding = f_gravity + f_normal
-        return Vector2.zero;
+        Vector2 f_sliding = f_gravity + f_normal;
+        return f_sliding;
     }
 
     // Vector2 GenerateForce_friction_static(Vector2 f_normal, Vector2 f_opposing, float frictionCoefficient_static);
-    public static Vector2 GenerateForce_Friction_Static()
+    public static Vector2 GenerateForce_Friction_Static(Vector2 f_normal, Vector2 f_opposing, float frictionCoefficient_static)
     {
         // f_friction_s = -f_opposing if less than max, else -coeff*f_normal (max amount is coeff*|f_normal|)
+        if (-f_opposing.magnitude < (frictionCoefficient_static * new Vector2(Mathf.Abs(f_normal.x), Mathf.Abs(f_normal.y)).magnitude))
+        {
+
+        }
+        Vector2 f_friction_s;
         return Vector2.zero;
     }
 
--- GamePhysics_FA19/Assets/Scripts/Physics/J_Physics.cs
+++ GamePhysics_FA19/Assets/Scripts/Physics/J_Physics.cs
@@ -8,17 +8,17 @@ public class J_Physics
 
     static public void UpdatePosition(ref Vector2 position, ref Vector2 velocity, ref Vector2 acceleration, float dt)
     {
-        // v(t+dt) = v(t) + a(t)dt
-        velocity += acceleration * dt;
         // x(t+dt) = x(t) + v(t+dt) + 1/2(a(t)(dt*dt))
         position += (velocity * dt) + (.5f * (acceleration * (dt * dt)));
+        // v(t+dt) = v(t) + a(t)dt
+        velocity += acceleration * dt;
     }
 
     static public void UpdateRotation(ref Vector3 rotation, ref Vector3 angularVelocity, ref Vector3 angularAcceleration, float dt)
     {
-        // v(t+dt) = v(t) + a(t)dt
-        angularVelocity += angularAcceleration * dt;
         // x(t+dt) = x(t) + v(t+dt) + 1/2(a(t)(dt*dt))
         rotation += (angularVelocity * dt) + (.5f * (angularAcceleration * (dt * dt)));
+        // v(t+dt) = v(t) + a(t)dt
+        angularVelocity += angularAcceleration * dt;
     }
 }
