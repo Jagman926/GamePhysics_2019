a5196fd87c3817dc2a4aac2869aca5f255afbcd4
Josh_Laptop <joshuagrazda@yahoo.com>
2019-10-08 21:04:36 -0400

    Player Screen Looping
    Added:
    - CheckEdgeOfView() to teleport player to opposite side of screen if exiting screen
    Changed:
    - UpdateRotation now clamps between 0-360 degrees
    - Adjusted camera in scene to be orthographic for 2D view

--- GamePhysics_FA19/Assets/Scripts/Player/PlayerController.cs
+++ GamePhysics_FA19/Assets/Scripts/Player/PlayerController.cs
@@ -10,6 +10,13 @@ public class PlayerController : MonoBehaviour
     private KeyCode keycode_forward = KeyCode.UpArrow;
     private KeyCode keyCode_reverse = KeyCode.DownArrow;
 
+    // Edge of Map Variables
+    private float edgeRight = 18.0f;
+    private float edgeLeft = -18.0f;
+    private float edgeTop = 10.3f;
+    private float edgeBot = -10.3f;
+    private float buffer = 0.1f;
+
     // Particle2D
     private Particle2D p2D;
 
@@ -36,6 +43,7 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        CheckEdgeOfView();
         UpdateMovement();
     }
 
@@ -60,5 +68,21 @@ public class PlayerController : MonoBehaviour
         {
             p2D.AddForceForward(-forceReverse2D);
         }
+    }
+
+    void CheckEdgeOfView()
+    {
+        // If moves off right side of view, move to left side
+        if (p2D.position.x > edgeRight + buffer)
+            p2D.position = new Vector2(edgeLeft - buffer, transform.position.y);
+        // If moves off left side of view, move to right side
+        else if (p2D.position.x < edgeLeft - buffer)
+            p2D.position = new Vector2(edgeRight + buffer, transform.position.y);
+        // If moves off top of view, move to bottom side
+        else if (p2D.position.y > edgeTop + buffer)
+            p2D.position = new Vector2(transform.position.x, edgeBot - buffer);
+        // If moves off bottom of view, move to top side
+        else if (p2D.position.y < edgeBot - buffer)
+            p2D.position = new Vector2(transform.position.x, edgeTop + buffer);
     }
 }
