ec1c7adb902791829e9a221c0a42e522d3209941
Josh_J101_08 <joshuagrazda@gmail.com>
2019-10-24 11:16:45 -0400

    J_Quaternion start
    Added:
    - Quaternion class

---
+++ GamePhysics_FA19/Assets/Scripts/Physics/J_Quaternion.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class J_Quaternion
+{
+    public float x, y, z, w;
+
+    public void SetQuaterntion(float newX, float newY, float newZ, float newW)
+    {
+        x = newX;
+        y = newY;
+        z = newZ;
+        w = newW;
+    }
+}
