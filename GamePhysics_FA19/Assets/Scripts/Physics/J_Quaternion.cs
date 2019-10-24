5c909e799536f240dd98d2f307c98c904f0b8d1e
Josh_J101_08 <joshuagrazda@gmail.com>
2019-10-24 11:20:43 -0400

    Quaternion starter functionality
    Added:
    - Functionality and zero quat

--- GamePhysics_FA19/Assets/Scripts/Physics/J_Quaternion.cs
+++ GamePhysics_FA19/Assets/Scripts/Physics/J_Quaternion.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class J_Quaternion
 {
     public float x, y, z, w;
+    public J_Quaternion zero = new J_Quaternion();
+
+    public J_Quaternion()
+    {
+        SetQuaterntion(0,0,0,0);
+    }
 
     public void SetQuaterntion(float newX, float newY, float newZ, float newW)
     {
