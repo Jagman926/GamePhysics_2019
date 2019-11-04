9d6bd363f8e5b2562e4a1cd97e5eccaff408128e
Josh_J101_08 <joshuagrazda@gmail.com>
2019-11-04 12:01:11 -0500

    Started OBB adjustments to 3D
    Changed:
    - Created 3D functionality for collision functions
    - Working on getting Min and Max extents

--- GamePhysics_FA19/Assets/Scripts/Physics/Collision3D/ObjectBoundingBoxCollisionHull3D.cs
+++ GamePhysics_FA19/Assets/Scripts/Physics/Collision3D/ObjectBoundingBoxCollisionHull3D.cs
@@ -36,22 +36,28 @@ public class ObjectBoundingBoxCollisionHull3D : CollisionHull3D
         Quaternion storedRotation = transform.rotation;
 
         // Determine extents
-        halfExtents = new Vector2(0.5f * particle.width, 0.5f * particle.height);
-        minExtent = new Vector2(center.x - halfExtents.x, center.y - halfExtents.y);
-        maxExtent = new Vector2(center.x + halfExtents.x, center.x + halfExtents.y);
+        halfExtents = new Vector3(0.5f * particle.width, 0.5f * particle.height, 0.5f * particle.length);
+        minExtent = new Vector3(center.x - halfExtents.x, center.y - halfExtents.y, center.z - halfExtents.z);
+        maxExtent = new Vector3(center.x + halfExtents.x, center.x + halfExtents.y, center.z + halfExtents.z);
 
         // Set rotation to identity
         transform.rotation = Quaternion.identity;
         // Transform points by world matrix
-        Vector2 corner1 = objectToWorldMatrix.MultiplyPoint3x4(new Vector2(halfExtents.x, halfExtents.y));
-        Vector2 corner2 = objectToWorldMatrix.MultiplyPoint3x4(new Vector2(halfExtents.x, -halfExtents.y));
-        Vector2 corner3 = objectToWorldMatrix.MultiplyPoint3x4(new Vector2(-halfExtents.x, halfExtents.y));
-        Vector2 corner4 = objectToWorldMatrix.MultiplyPoint3x4(new Vector2(-halfExtents.x, -halfExtents.y));
-        // Calculate min/max rotated points
-        minExtent_Rotated.x = Mathf.Min(corner1.x, Mathf.Min(corner2.x, Mathf.Min(corner3.x, corner4.x)));
+        Vector3 corner1 = objectToWorldMatrix.MultiplyPoint3x4(new Vector3(halfExtents.x, halfExtents.y, halfExtents.z));
+        Vector3 corner2 = objectToWorldMatrix.MultiplyPoint3x4(new Vector3(halfExtents.x, -halfExtents.y, halfExtents.z));
+        Vector3 corner3 = objectToWorldMatrix.MultiplyPoint3x4(new Vector3(-halfExtents.x, halfExtents.y, halfExtents.z));
+        Vector3 corner4 = objectToWorldMatrix.MultiplyPoint3x4(new Vector3(-halfExtents.x, -halfExtents.y, halfExtents.z));
+        Vector3 corner5 = objectToWorldMatrix.MultiplyPoint3x4(new Vector3(halfExtents.x, halfExtents.y, -halfExtents.z));
+        Vector3 corner6 = objectToWorldMatrix.MultiplyPoint3x4(new Vector3(halfExtents.x, -halfExtents.y, -halfExtents.z));
+        Vector3 corner7 = objectToWorldMatrix.MultiplyPoint3x4(new Vector3(-halfExtents.x, halfExtents.y, -halfExtents.z));
+        Vector3 corner8 = objectToWorldMatrix.MultiplyPoint3x4(new Vector3(-halfExtents.x, -halfExtents.y, -halfExtents.z));
+        // Calculate min/max rotated points 
+        minExtent_Rotated.x = Mathf.Min(corner1.x, Mathf.Min(corner2.x, Mathf.Min(corner3.x, corner4.x))); // TODO: Need to find min of 8 points
         minExtent_Rotated.y = Mathf.Min(corner1.y, Mathf.Min(corner2.y, Mathf.Min(corner3.y, corner4.y)));
-        maxExtent_Rotated.x = Mathf.Max(corner1.x, Mathf.Max(corner2.x, Mathf.Max(corner3.x, corner4.x)));
+        minExtent_Rotated.z = Mathf.Min(corner1.y, Mathf.Min(corner2.y, Mathf.Min(corner3.y, corner4.y)));
+        maxExtent_Rotated.x = Mathf.Max(corner1.x, Mathf.Max(corner2.x, Mathf.Max(corner3.x, corner4.x))); // TODO: Need to find max of 8 points
         maxExtent_Rotated.y = Mathf.Max(corner1.y, Mathf.Max(corner2.y, Mathf.Max(corner3.y, corner4.y)));
+        maxExtent_Rotated.z = Mathf.Max(corner1.y, Mathf.Max(corner2.y, Mathf.Max(corner3.y, corner4.y)));
 
         // Reset rotation
         transform.rotation = storedRotation;
@@ -65,7 +71,6 @@ public class ObjectBoundingBoxCollisionHull3D : CollisionHull3D
             case CollisionHullType3D.hull_circle:
                 if (TestCollisionVsCircle((CircleCollisionHull3D)other, ref c))
                 {
-
                     if (c.status)
                     {
                         c.status = false;
@@ -78,8 +83,6 @@ public class ObjectBoundingBoxCollisionHull3D : CollisionHull3D
                         //clearContacts(ref c); //Clears the information used after contacts have been resolved
                         c.status = true;
                     }
-
-
                     Debug.Log(gameObject.name + " Colliding with " + other.name);
 
                     colliding = true;
@@ -111,12 +114,10 @@ public class ObjectBoundingBoxCollisionHull3D : CollisionHull3D
             default:
                 break;
         }
-        /*
         if (colliding)
             renderer.material = mat_red;
         else
             renderer.material = mat_green;
-        */
 
         return colliding;
     }
@@ -149,11 +150,11 @@ public class ObjectBoundingBoxCollisionHull3D : CollisionHull3D
         // 3. For both test, and if both true, pass
 
         // Other object multiplied by inverse world matrix
-        Vector2 obb1_maxExtent_transInv = other.transform.worldToLocalMatrix.MultiplyPoint(other.maxExtent);
-        Vector2 obb1_minExtent_transInv = other.transform.worldToLocalMatrix.MultiplyPoint(other.minExtent);
+        Vector3 obb1_maxExtent_transInv = other.transform.worldToLocalMatrix.MultiplyPoint(other.maxExtent);
+        Vector3 obb1_minExtent_transInv = other.transform.worldToLocalMatrix.MultiplyPoint(other.minExtent);
         // This object multiplied by inverse world matrix
-        Vector2 obb2_maxExtent_transInv = transform.worldToLocalMatrix.MultiplyPoint(maxExtent);
-        Vector2 obb2_minExtent_transInv = transform.worldToLocalMatrix.MultiplyPoint(minExtent);
+        Vector3 obb2_maxExtent_transInv = transform.worldToLocalMatrix.MultiplyPoint(maxExtent);
+        Vector3 obb2_minExtent_transInv = transform.worldToLocalMatrix.MultiplyPoint(minExtent);
 
         obb1_maxExtent_transInv += center;
         obb1_minExtent_transInv += center;
@@ -163,12 +164,16 @@ public class ObjectBoundingBoxCollisionHull3D : CollisionHull3D
         if (obb1_maxExtent_transInv.x > minExtent.x &&
             obb1_minExtent_transInv.x < maxExtent.x &&
             obb1_maxExtent_transInv.y > minExtent.y &&
-            obb1_minExtent_transInv.y < maxExtent.y)
+            obb1_minExtent_transInv.y < maxExtent.y &&
+            obb1_maxExtent_transInv.z > minExtent.z &&
+            obb1_minExtent_transInv.z < maxExtent.z)
         {
             if (obb2_maxExtent_transInv.x > other.minExtent.x &&
                 obb2_minExtent_transInv.x < other.maxExtent.x &&
                 obb2_maxExtent_transInv.y > other.minExtent.y &&
-                obb2_minExtent_transInv.y < other.maxExtent.y)
+                obb2_minExtent_transInv.y < other.maxExtent.y &&
+                obb2_maxExtent_transInv.z > other.minExtent.z &&
+                obb2_minExtent_transInv.z < other.maxExtent.z)
                 return true;
         }
 
