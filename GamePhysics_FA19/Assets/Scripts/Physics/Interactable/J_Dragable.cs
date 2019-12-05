bedfa2c14d4ccd6be69b8a662e82c82e31f0eabb
Josh_Main <joshuagrazda@gmail.com>
2019-12-04 20:45:19 -0500

    Created Start of Jenga Game
    Added:
    - Drag Scene (Test scene for new scripts)
    - Materials for objects
    - Physics material for blocks
    - Jenga stack parented to table
    - Rotate camera on 'A' and 'D'
    - Click on block and pull to move towards camera
    Changed:
    - Physics settings (Gravity set to 11)
    - Lighting (Auto Generate)

---
+++ GamePhysics_FA19/Assets/Scripts/Physics/Interactable/J_Dragable.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class J_Dragable : MonoBehaviour
+{
+    /*
+    References:
+    Jayanam - Unity Tutorial: Drag Gameobject with Mouse
+     */
+
+    // Main Camera
+    Camera mainCamera = null;
+
+    // RigidBody
+    Rigidbody rb = null;
+
+    // Drag Coords
+    RaycastHit hit;
+    private Vector3 dragOffset;
+    private float zCoord;
+
+    void Awake()
+    {
+        mainCamera = Camera.main;
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// OnMouseDown is called when the user has pressed the mouse button while over the GUIElement or Collider.
+    /// </summary>
+    void OnMouseDown()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        // Cast ray and get first gameObject hit
+        Physics.Raycast(ray, out hit);
+        Debug.Log("This hit at " + hit.point);
+        //zCoord = mainCamera.WorldToScreenPoint(gameObject.transform.position).z;
+        //dragOffset = gameObject.transform.position - GetMouseWorldPos();
+    }
+
+    /// <summary>
+    /// OnMouseDrag is called when the user has clicked on a GUIElement or Collider and is still holding down the mouse.
+    /// </summary>
+    void OnMouseDrag()
+    {
+        //transform.position = GetMouseWorldPos() + dragOffset;
+        // Determine force
+        Vector3 pullForce;
+        pullForce = (GetMouseWorldPos() - hit.point);
+        rb.AddForce(pullForce.normalized * pullForce.magnitude);
+    }
+
+    Vector3 GetMouseWorldPos()
+    {
+        // pixel coords (x,y)
+        Vector3 mousePoint = Input.mousePosition;
+        // z coords of game object on screen
+        mousePoint.z = zCoord;
+
+        return mainCamera.ScreenToWorldPoint(mousePoint);
+    }
+}
---
+++ GamePhysics_FA19/Assets/Scripts/Physics/Interactable/J_Rotatable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class J_Rotatable : MonoBehaviour
+{
+    // Main Camera
+    Camera mainCamera = null;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, 1, 0));
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, -1, 0));
+        }
+    }
+}
