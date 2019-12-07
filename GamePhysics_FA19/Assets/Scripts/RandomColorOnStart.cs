368e5545fb06469e8ad66958c9bc6f71278a7abd
Josh_Main <joshuagrazda@gmail.com>
2019-12-07 13:38:54 -0500

    Spicey Scene Updates
    Added:
    - New materials and texture
    - Particle system for fireplace
    Changed:
    - Random color script to tan and brown
    - Moved around stuff in drag scene

--- GamePhysics_FA19/Assets/Scripts/RandomColorOnStart.cs
+++ GamePhysics_FA19/Assets/Scripts/RandomColorOnStart.cs
@@ -6,6 +6,6 @@ public class RandomColorOnStart : MonoBehaviour
 {
     void Start()
     {
-        GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        GetComponent<Renderer>().material.color = Random.ColorHSV(0.15f, 0.16f, .1f, .2f, 0.7f, 1f);
     }
 }
