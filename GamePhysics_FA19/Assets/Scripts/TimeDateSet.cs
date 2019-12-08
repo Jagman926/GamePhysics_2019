dd25549ffe7e9f78e421eb643edf7e754993b7fc
Josh_Main <joshuagrazda@gmail.com>
2019-12-07 22:30:50 -0500

    More Props and Stuff
    Added:
    - TV with Video
    - Lamp and Picture
    - Phone with TimeDate script
    - Materials
    Changed:
    - Physics increased again
    - adjusted materials

---
+++ GamePhysics_FA19/Assets/Scripts/TimeDateSet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimeDateSet : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI timeText = null;
+    [SerializeField]
+    TextMeshProUGUI dateText = null;
+
+    void Update()
+    {
+        timeText.text = System.DateTime.Now.TimeOfDay.Hours + ":" + System.DateTime.Now.TimeOfDay.Minutes;
+        dateText.text = System.DateTime.Now.ToLongDateString().ToString();
+    }
+}
