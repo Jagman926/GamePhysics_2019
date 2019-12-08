55562bfe1c9dc84d3aae76c8ac598c32b73b24c2
Josh_Main <joshuagrazda@gmail.com>
2019-12-07 23:31:24 -0500

    Game Loop Added
    Added:
    - Button UI for reset
    - Phone has UI for reset when JengaFallTest is true
    - JengaMaster script for managing end game and reset
    Changed:
    - Build order for scene reset

---
+++ GamePhysics_FA19/Assets/Scripts/JengaFallTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JengaFallTest : MonoBehaviour
+{
+    [SerializeField]
+    JengaManager jengaManager;
+
+    [SerializeField]
+    float FallPositionMin = 0.0f;
+
+    void Update()
+    {
+        if (transform.position.y < FallPositionMin)
+        {
+            jengaManager.EndGame();
+            Debug.Log("End");
+        }
+    }
+}
---
+++ GamePhysics_FA19/Assets/Scripts/JengaManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class JengaManager : MonoBehaviour
+{
+    [SerializeField]
+    bool endGame = false;
+    bool resetGame = false;
+
+    [SerializeField]
+    GameObject resetButton = null;
+
+    void Awake()
+    {
+        resetButton.SetActive(false);
+    }
+
+    void Start()
+    {
+        endGame = false;
+        resetGame = false;
+    }
+
+    void Update()
+    {
+        if(endGame && resetGame)
+            SceneManager.LoadScene(0);
+    }
+
+    public void EndGame()
+    {
+        endGame = true;
+        resetButton.SetActive(true);
+    }
+
+    public void ResetGame()
+    {
+        resetGame = true;
+    }
+}
